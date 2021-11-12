3d0131dadf2a48c43a3dace17f7f9a99e2c59307
Gabriel Falcão (he/him/his) <gabriel.falcao@nytimes.com>
2021-11-12 22:39:59 +0100

    copy webpack config from electron-react-boilerplate

--- webpack.custom.config.js
+++ webpack.custom.config.js
@@ -1,25 +1,33 @@
 //const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 const path = require("path");
+const fs = require("fs");
+const { dependencies } = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'package.json')));
+
+const srcPath = path.resolve(__dirname, "src");
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.jsx?/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
-  resolve: {
-    alias: {
-      "renderer": path.resolve(__dirname, "src/renderer"),
-      "main": path.resolve(__dirname, "src/main"),
+    externals: [...Object.keys(dependencies || {})],
+    stats: "errors-only",
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ],
     },
-  },
-  plugins: [
-    //new MonacoWebpackPlugin()
-  ],
+    resolve: {
+        extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+        modules: [srcPath, "node_modules"],
+        alias: {
+            renderer: path.resolve(__dirname, "src/renderer"),
+            main: path.resolve(__dirname, "src/main"),
+        },
+    },
+    plugins: [
+        //new MonacoWebpackPlugin()
+    ],
 };
