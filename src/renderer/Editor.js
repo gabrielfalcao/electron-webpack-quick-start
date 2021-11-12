2aed40920d1b07cd0e1330f29a3673f0164d519c
Gabriel Falcão (he/him/his) <gabriel.falcao@nytimes.com>
2021-11-13 00:27:30 +0100

    bosta

--- src/renderer/Editor.js
+++ src/renderer/Editor.js
@@ -1,8 +1,4 @@
-import React, {
-    useEffect,
-    useRef,
-    useState,
-} from "react";
+import React, { useEffect, useRef, useState } from "react";
 //import * as monaco from 'monaco-editor';
 
 /* theme="vs-dark"
@@ -11,15 +7,23 @@ import React, {
  * value={code.content || "#!/usr/bin/env bash\n"}
  *  */
 export const defaultOptions = {
-    theme: 'vs-dark', height: '100%', width: '100%', language: 'shell', value: "empty"
-}
+  theme: "vs-dark",
+  height: "100%",
+  width: "100%",
+  language: "shell",
+  value: "empty",
+};
 export default function Editor(options = defaultOptions) {
-    const element = useRef(null);
-    const [editor, setEditor] = useState(null);
+  const element = useRef(null);
+  const [editor, setEditor] = useState(null);
 
-    if (!editor && element) {
-        //setEditor(monaco.editor.create(element, {...defaultOptions, ...options}))
-        element.current.innerHTML = options.value
-    }
-    return <pre contentEditable ref={element}></pre>
+  if (!editor && element) {
+    //setEditor(monaco.editor.create(element, {...defaultOptions, ...options}))
+  }
+
+  return (
+    <pre contentEditable ref={element}>
+      {options.value}
+    </pre>
+  );
 }
